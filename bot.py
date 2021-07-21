b4598d886bda550a86955247b4d563b7f2202687
drop0705 <85348432+drop0705@users.noreply.github.com>
2021-07-21 20:54:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,35 @@
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler
-from telegram.ext import Filters
-from settings import TG_TOKEN, TG_API_URL
+import requests
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from settings import TG_TOKEN
+from telebot import apihelper
+from handlers import *
 
-
-def sms(bot, update):
-    print('Кто-то отправил команду /start. Что мне делать?') #вывод сообщения в консоль, когда нажимают /start
-    bot.message.reply_text('Здравствуйте, {}! я бот, а ты иди нахуй чмошник обрыганный! \n'
-                            'Я пока не умею разговаривать, но ебало тебе быстро снесу'.format(bot.message.chat.first_name))
-    print(bot.message)
-
-
-def parrot(bot, update):
-    print(bot.message.text)
-    bot.message.reply_text(bot.message.text)
-
+apihelper.proxy = {"https": "129.226.144.129:19000"}
 
 def main():
-    my_bot = Updater(TG_TOKEN, TG_API_URL, use_context=True)
-    my_bot.dispatcher.add_handler(CommandHandler('start', sms)) # обработчик команды
+    my_bot = Updater(TG_TOKEN)
+    my_bot.dispatcher.add_handler(CommandHandler('start', sms))  # обработчик команды
+    my_bot.dispatcher.add_handler(MessageHandler(Filters.regex('начать'), sms))
+    my_bot.dispatcher.add_handler(MessageHandler(Filters.regex('Анекдот'), get_anecdote))
+    my_bot.dispatcher.add_handler(MessageHandler(Filters.contact, get_contact))
+    my_bot.dispatcher.add_handler(MessageHandler(Filters.location, get_location))
+    my_bot.dispatcher.add_handler(ConversationHandler(entry_points=[MessageHandler(Filters.regex('Заполнить анкету'), anketa_start)],
+                                                      states={
+                                                          "user_name": [MessageHandler(Filters.text, anketa_get_name)],
+                                                          "user_age": [MessageHandler(Filters.text, anketa_get_age)],
+                                                          "evalution": [MessageHandler(Filters.regex('1|2|3|4|5'), anketa_get_evalution)],
+                                                          "comment": [MessageHandler(Filters.regex('Пропустить'), anketa_exit_comment),
+                                                                      MessageHandler(Filters.text, anketa_comment)]
+                                                      },
+                                                      fallbacks=[MessageHandler(
+                                                          Filters.text | Filters.video | Filters.photo | Filters.document, dontknow)]
+                                                      )
+                                  )
     my_bot.dispatcher.add_handler(MessageHandler(Filters.text, parrot))
-    my_bot.start_polling() # проверяет о наличие сообщений в ТГ
-    my_bot.idle() # бот будет работать, пока его не остановят
+    my_bot.start_polling()  # проверяет о наличие сообщений в ТГ
+    my_bot.idle()  # бот будет работать, пока его не остановят
+
 
 # вызываем функцию запсука бота main
-main()
+if __name__ == "__main__":
+    main()
